83016a4f17c4b9d8676298a380078788478de34f
g00glen00b <me@g00glen00b.be>
2018-08-08 15:59:27 +0200

    Implemented comments

--- angular-websockets-client/src/app/comment/comment.module.ts
+++ angular-websockets-client/src/app/comment/comment.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentListComponent } from './comment-list/comment-list.component';
 import { CommentListItemComponent } from './comment-list/comment-list-item/comment-list-item.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   imports: [
+    CoreModule,
     CommonModule
   ],
   declarations: [CommentListComponent, CommentListItemComponent],
