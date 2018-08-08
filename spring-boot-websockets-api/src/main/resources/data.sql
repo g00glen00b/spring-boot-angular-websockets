e6113bf685c738ed6ff2c18769c6c94d94a09a95
g00glen00b <me@g00glen00b.be>
2018-08-08 15:44:36 +0200

    Created DDL and DML

---
+++ spring-boot-websockets-api/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into author (id, username) values (1, 'g00glen00b'), (2, 'john.doe');
+insert into post (id, title, content, author_id, posted_at) values
+  (1, 'Welcome to this community', 'This is a small introduction text to the wonderful community of postit. Just add new posts by opening the link at the left navigation. To view a post, you can click its title and post comments to it.', 1, '2018-08-01 00:00:00'),
+  (2, 'I hate this', 'I don''t like this community and I''m going back to Reddit. Cya later!', 2, '2018-08-03 00:00:00');
+insert into comment (id, content, author_id, post_id, posted_at) values
+  (1, 'This community looks interesting, it might be even better than Reddit!', 2, 1, '2018-08-01 01:00:00'),
+  (2, 'I agree with you @john.doe', 1, 1, '2018-08-01 02:00:00'),
+  (3, 'Sorry to see you go!', 1, 2, '2018-08-04 00:00:00');
---
+++ spring-boot-websockets-api/src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+create table author (
+  id        integer identity primary key,
+  username  varchar(64) unique not null);
+
+create table post (
+  id        integer identity primary key,
+  title     varchar(128) not null,
+  content   varchar(1024) not null,
+  posted_at timestamp not null,
+  author_id integer not null);
+
+create table comment (
+  id        integer identity primary key,
+  content   varchar(2014) not null,
+  posted_at timestamp not null,
+  post_id   integer not null,
+  author_id integer not null);
+
+alter table post add foreign key (author_id) references author(id);
+alter table comment add foreign key (author_id) references author(id);
+alter table comment add foreign key (post_id) references post(id);
