d3097b14fedcbbe2dece0413bdb0f7770b6099e9
g00glen00b <me@g00glen00b.be>
2018-08-08 15:43:32 +0200

    Implemented websocket configuration

---
+++ spring-boot-websockets-api/src/main/java/be/g00glen00b/apps/web/WebSocketConfiguration.java
@@ -0,0 +1,30 @@
+package be.g00glen00b.apps.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@ConfigurationProperties
+@EnableWebSocketMessageBroker
+@EnableConfigurationProperties(WebSocketProperties.class)
+@AllArgsConstructor
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+	private WebSocketProperties properties;
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker(properties.getTopicPrefix());
+		registry.setApplicationDestinationPrefixes(properties.getApplicationPrefix());
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint(properties.getEndpoint()).setAllowedOrigins(properties.getAllowedOrigins()).withSockJS();
+	}
+}
---
+++ spring-boot-websockets-api/src/main/java/be/g00glen00b/apps/web/WebSocketProperties.java
@@ -0,0 +1,25 @@
+package be.g00glen00b.apps.web;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("app.websocket")
+public class WebSocketProperties {
+	/**
+	 * Prefix used for WebSocket destination mappings
+	 */
+	private String applicationPrefix = "/topic";
+	/**
+	 * Prefix used by topics
+	 */
+	private String topicPrefix = "/toic";
+	/**
+	 * Endpoint that can be used to connect to
+	 */
+	private String endpoint = "/live";
+	/**
+	 * Allowed origins
+	 */
+	private String[] allowedOrigins = new String[0];
+}
