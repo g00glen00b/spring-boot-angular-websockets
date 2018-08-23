a8dcad2e0c8e4684033383d833a17b80c17f6253
g00glen00b <me@g00glen00b.be>
2018-08-23 19:17:32 +0200

    Implemented dynamically changing page title

--- angular-websockets-client/src/app/post/post-info-page/post-info-page.component.ts
+++ angular-websockets-client/src/app/post/post-info-page/post-info-page.component.ts
@@ -1,30 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
 import { ActivatedRoute } from '@angular/router';
 import { map, share, switchMap } from 'rxjs/operators';
 import { PostInfo } from '../post-info';
 import { CommentService } from '../../comment/comment.service';
 import { Observable } from 'rxjs/internal/Observable';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-info-page',
   templateUrl: './post-info-page.component.html'
 })
-export class PostInfoPageComponent implements OnInit {
+export class PostInfoPageComponent implements OnInit, OnDestroy {
   post: PostInfo;
   newComment: string;
 
-  constructor(private service: PostService, private commentService: CommentService, private activatedRoute: ActivatedRoute) { }
+  constructor(private service: PostService, private commentService: CommentService, private activatedRoute: ActivatedRoute, private titleService: Title) { }
 
   ngOnInit() {
     const postId: Observable<number> = this.activatedRoute.params.pipe(map(params => params['id']));
     postId.pipe(share(), switchMap(id => this.commentService.onComment(id))).subscribe(comment => this.post.comments.push(comment));
-    postId.pipe(share(), switchMap(id => this.service.findOne(id))).subscribe(post => this.post = post);
+    postId.pipe(share(), switchMap(id => this.service.findOne(id))).subscribe(post => {
+      this.post = post;
+      this.titleService.setTitle(`postit - ${post.title}`);
+    });
   }
 
   addComment(content: string) {
     this.commentService.addComment(this.post.id, {content, author: {id: 1}});
     this.newComment = '';
+  }
+
+  ngOnDestroy(): void {
+    this.titleService.setTitle('postit');
   }
 
 }
